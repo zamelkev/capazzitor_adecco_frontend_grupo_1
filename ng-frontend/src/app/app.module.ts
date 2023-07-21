64d4468822b5bb8bcf7ab69a9deb80d292de6874
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-07-21 11:56:02 +0200

    update / Files

--- ng-frontend/src/app/app.module.ts
+++ ng-frontend/src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Firebase services + environment module
+// import { AngularFireModule } from '@angular/fire/compat';
+// import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+// import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+// import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+// import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+// Auth service
+
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AboutUsModule } from './about-us/about-us.module';
-import { DashboardModule } from './dashboard/dashboard.module';
+// import { DashboardModule } from './dashboard/dashboard.module';
 import { MainComponentsModule } from './main-components/main-components.module';
 import { ViewsModule } from './views/views.module';
+import { UsersModule } from './users/users.module';
 
 @NgModule({
   declarations: [
@@ -17,13 +26,19 @@ import { ViewsModule } from './views/views.module';
   ],
   imports: [
     AboutUsModule,
-    DashboardModule,
+    // DashboardModule,
     MainComponentsModule,
     ViewsModule,
+    UsersModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    RouterModule
+    RouterModule,
+    // AngularFireModule.initializeApp(environment.firebase),
+    // AngularFireAuthModule,
+    // AngularFirestoreModule,
+    // AngularFireStorageModule,
+    // AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
