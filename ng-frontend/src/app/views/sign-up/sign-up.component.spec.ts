6852d4cd41a46ae0de66ee9495574df9ed181fe7
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-07-21 11:54:41 +0200

    feature / Adding Modules & Components

--- ng-frontend/src/app/views/sign-up/sign-up.component.spec.ts
+++ ng-frontend/src/app/views/sign-up/sign-up.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+<<<<<<<< HEAD:ng-frontend/src/app/views/sign-up/sign-up.component.spec.ts
 import { SignUpComponent } from './sign-up.component';
 
 describe('SignUpComponent', () => {
@@ -11,6 +12,19 @@ describe('SignUpComponent', () => {
       declarations: [SignUpComponent]
     });
     fixture = TestBed.createComponent(SignUpComponent);
+========
+import { OfferFormComponent } from './offer-form.component';
+
+describe('OfferFormComponent', () => {
+  let component: OfferFormComponent;
+  let fixture: ComponentFixture<OfferFormComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [OfferFormComponent]
+    });
+    fixture = TestBed.createComponent(OfferFormComponent);
+>>>>>>>> 303b384 (feature / Adding Modules & Components):ng-frontend/src/app/views/offer-form/offer-form.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
