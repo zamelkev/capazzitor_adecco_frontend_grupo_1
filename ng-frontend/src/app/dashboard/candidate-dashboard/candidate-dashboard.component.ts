6305702c43fcce7df49b124bb217610a6b955a52
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-07-21 11:54:41 +0200

    feature / Adding Modules & Components

--- ng-frontend/src/app/dashboard/candidate-dashboard/candidate-dashboard.component.ts
+++ ng-frontend/src/app/dashboard/candidate-dashboard/candidate-dashboard.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-candidate-dashboard',
   templateUrl: './candidate-dashboard.component.html',
   styleUrls: ['./candidate-dashboard.component.css']
 })
-export class CandidateDashboardComponent {
-
+export class CandidateDashboardComponent implements OnInit {
+  usuario!: User;
+  constructor() {}
+  ngOnInit(): void {}
 }
---
+++ ng-frontend/src/app/views/offer-detail/offer-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offer-detail',
+  templateUrl: './offer-detail.component.html',
+  styleUrls: ['./offer-detail.component.css']
+})
+export class OfferDetailComponent {
+
+}
