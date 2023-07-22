bac256a5fe2b8c7b5aae65698067dc1df160d5fb
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-07-22 09:50:24 +0200

    fixed / Injection Browser Issue

--- ng-frontend/src/app/app-routing.module.ts
+++ ng-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Components Import
+import { AboutUsComponent } from './about-us/about-us/about-us.component';
+import { AdminDashboardComponent } from './dashboard/admin-dashboard/admin-dashboard.component';
+import { CandidateDashboardComponent } from './dashboard/candidate-dashboard/candidate-dashboard.component';
+import { CompanyDashboardComponent } from './dashboard/company-dashboard/company-dashboard.component';
+import { CurrentNewsComponent } from './views/current-news/current-news.component';
+import { HomeComponent } from './views/home/home.component';
+import { SignInComponent } from './account/sign-in/sign-in.component';
+import { SignUpComponent } from './account/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './account/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './account/verify-email/verify-email.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'current-news', component: CurrentNewsComponent },
+  
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
+  { path: 'candidate-dashboard', component: CandidateDashboardComponent },
+  { path: 'company-dashboard', component: CompanyDashboardComponent },
+
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'register-user', component: SignUpComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'verify-email-address', component: VerifyEmailComponent },
+  
+  // { path: '**', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ng-frontend/src/app/app.module.ts
+++ ng-frontend/src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-// Firebase services + environment module
-// import { AngularFireModule } from '@angular/fire/compat';
-// import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-// import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-// import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-// import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-// Auth service
-import { AuthService } from './services/auth.service';
-// import { AlertService } from './services/alert.service';
-import { UserService } from './services/user.service';
-
+// Main
 import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Firebase services + environment module
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuth, AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+// import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+// import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+// Forms
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Services
+import { AuthService } from './services/auth.service';
+// import { AlertService } from './services/alert.service';
+// import { UserService } from './services/user.service';
+
+// Modules
 import { AboutUsModule } from './about-us/about-us.module';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { MainComponentsModule } from './main-components/main-components.module';
@@ -24,10 +30,24 @@ import { ViewsModule } from './views/views.module';
 import { environment } from '../environments/environment';
 import { AccountModule } from './account/account.module';
 import { UsersModule } from './users/users.module';
+
+// Firebase
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyD5sMKznzYQst-vDhTbUDZKWrHPFn8Fm0U",
+  authDomain: "portal-empleo-7f519.firebaseapp.com",
+  databaseURL: "https://portal-empleo-7f519-default-rtdb.firebaseio.com",
+  projectId: "portal-empleo-7f519",
+  storageBucket: "portal-empleo-7f519.appspot.com",
+  messagingSenderId: "700855171868",
+  appId: "1:700855171868:web:33633e3bbe5a5d674bd8d9",
+  measurementId: "G-F067FM1D9W"
+};
 
 @NgModule({
   declarations: [
@@ -50,7 +70,7 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     provideDatabase(() => getDatabase()),
     provideFirestore(() => getFirestore()),
   ],
-  providers: [],
+  providers: [AuthService, { provide: FIREBASE_OPTIONS, useValue: environment.firebase }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
