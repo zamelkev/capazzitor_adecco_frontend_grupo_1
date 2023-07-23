f780cea4e527f734103ab6222eced7eecf35bcb6
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-07-23 12:25:05 +0200

    fixed feature / Navbar Nenus - Add options and router links - Create use-guides view

--- ng-frontend/src/app/main-components/header/header.component.ts
+++ ng-frontend/src/app/main-components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 
 @Component({
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  
+}
  
