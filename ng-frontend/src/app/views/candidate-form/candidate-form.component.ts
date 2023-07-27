b515a58a5ce8727ea430bf1a022c89a2e071f055
enekogs <enekogalvan@gmail.com>
2023-07-27 09:40:07 +0200

    featureEneko: starting with form

--- ng-frontend/src/app/views/candidate-form/candidate-form.component.ts
+++ ng-frontend/src/app/views/candidate-form/candidate-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 
 @Component({
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./candidate-form.component.css']
 })
 export class CandidateFormComponent {
-
+  ngOnInit(): void {}
 }
