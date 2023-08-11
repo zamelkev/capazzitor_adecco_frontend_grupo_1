ae171d2433370dccda26c456dc65e3fc15e7cbbf
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-08-11 19:08:24 +0200

    sorted / Sort & Organize Project Components

--- ng-frontend/src/app/app-routing.module.ts
+++ ng-frontend/src/app/app-routing.module.ts
@@ -3,13 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { IsLoggedInGuard } from './services/guards/is-logged-in.guard';
 import { HomeComponent } from './views/home/home.component';
-import { SignInComponent } from './views/sign-in/sign-in.component';
-import { SignUpComponent } from './views/sign-up/sign-up.component';
-import { ForgotPasswordComponent } from './views/forgot-password/forgot-password.component';
-import { VerifyEmailComponent } from './views/verify-email/verify-email.component';
+import { SignInComponent } from './views/auth/sign-in/sign-in.component';
+import { SignUpComponent } from './views/auth/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './views/auth/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './views/auth/verify-email/verify-email.component';
 import { UseGuidesComponent } from './views/use-guides/use-guides.component';
 import { ViewsModule } from './views/views.module';
-import { CompanyDashboardComponent } from './main-components/company-dashboard/company-dashboard.component';
+import { CompanyDashboardComponent } from './main-components/dashboard/company-dashboard/company-dashboard.component';
 
 const routes: Routes = [
   { path: '', /*canLoad: [IsLoggedInGuard],*/ component: HomeComponent },
--- ng-frontend/src/app/main-components/bottom-sheet/bottom-sheet.component.ts
+++ ng-frontend/src/app/main-components/bottom-sheet/bottom-sheet.component.ts
@@ -4,7 +4,7 @@ import {
 } from '@angular/material/bottom-sheet';
 import {MatListModule} from '@angular/material/list';
 import {MatButtonModule} from '@angular/material/button';
-import { OfferDetailComponent } from '../offer-detail/offer-detail.component';
+import { OfferDetailComponent } from '../offer/offer-detail/offer-detail.component';
 @Component({
   selector: 'app-bottom-sheet',
   templateUrl: './bottom-sheet.component.html',
