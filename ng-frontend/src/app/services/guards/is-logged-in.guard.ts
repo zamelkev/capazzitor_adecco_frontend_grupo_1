0a89bde83ffad014ed61d7484716691de724f8e6
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-08-16 20:13:26 +0200

    in progress / Role Based Auth - Ng Switch implementation / Issue Reading User Role and other entity variables

--- ng-frontend/src/app/services/guards/is-logged-in.guard.ts
+++ ng-frontend/src/app/services/guards/is-logged-in.guard.ts
@@ -1,26 +1,17 @@
-// import { Injectable } from '@angular/core';
-// import { CanLoad, Router, UrlTree } from '@angular/router';
-// import { map, Observable } from 'rxjs';
-// import { AuthService } from '../auth.service';
+import { Injectable } from '@angular/core';
+import { CanLoad, Router, UrlTree } from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AuthService } from '../auth.service';
 
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class IsLoggedInGuard implements CanLoad {
-//   constructor(private authService: AuthService, private router: Router) {}
+@Injectable({
+  providedIn: 'root',
+})
+export class IsLoggedInGuard implements CanLoad {
+  constructor(private authService: AuthService, private router: Router) {}
 
-<<<<<<< HEAD
   canLoad(): Observable<boolean | UrlTree> {
     return this.authService.isLoggedIn$.pipe(
       map((isLoggedIn) => isLoggedIn || this.router.createUrlTree(['../../login']))
     );
   }
 }
-=======
-//   canLoad(): Observable<boolean | UrlTree> {
-//     return this.authService.isLoggedIn$.pipe(
-//       map((isLoggedIn) => isLoggedIn || this.router.createUrlTree(['../../login']))
-//     );
-//   }
-// }
->>>>>>> 7f8b774 (in progress to fix / Fixing firebase/firestore permission issue:)
