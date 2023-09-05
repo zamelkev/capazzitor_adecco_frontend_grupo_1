409b317440415d51d9a77e5c721877191a91ec2d
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-09-05 20:24:07 +0200

    feature / Initialize MeetUs & Events Views

---
+++ ng-frontend/src/app/views/meet-us/meet-us.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-meet-us',
+  templateUrl: './meet-us.component.html',
+  styleUrls: ['./meet-us.component.css']
+})
+export class MeetUsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
