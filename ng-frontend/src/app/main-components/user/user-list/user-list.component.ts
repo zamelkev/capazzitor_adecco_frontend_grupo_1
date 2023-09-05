d91f4e41fa3c2b8229ac389f1d36a1ae404f13c7
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-09-05 10:35:52 +0200

    feature : Admin Dashboard / in progress - to improve / to fix - Styles & Functionality

--- ng-frontend/src/app/main-components/user/user-list/user-list.component.ts
+++ ng-frontend/src/app/main-components/user/user-list/user-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgIterable, OnInit } from '@angular/core';
+import { Observer } from '@firebase/util';
+import { Observable } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -9,28 +11,31 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class UserListComponent implements OnInit{
 
-  users: any[] = [];
+  users: User[] /*| Observable<User[]>*/ | NgIterable<any[]> | null | undefined = [];
+
   panelOpenState = false;
 
-  constructor(authService?: AuthService) {
-    // this.users = this.users = [
-    //   {id: 1, nombreSocial: "User1" },
-    //   {id: 2, nombreSocial: "User2" },
-    //   {id: 3, nombreSocial: "User2" },
-    //   {id: 4, nombreSocial: "User1" },
-    //   {id: 5, nombreSocial: "User2" },
-    //   {id: 6, nombreSocial: "User2" },
-    // ];
-  
+  constructor(private authService: AuthService) {
+    this.users = [
+      {
+      uid: 1234,
+      email: 'prueba@prueba.es',
+      displayName: 'Prueba',
+      password: '1234',
+      role: 'candidate',
+      photoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
+      emailVerified: true,
+      }
+    ];
   }
 
   ngOnInit(): void {
-    // this.authService.getUsers().subscribe((users: any[]) => {
-    //   this.users = users;
-    // })
+    this.authService?.getUsers().subscribe( users => {
+      this.users = users;
+    })
   }
 
-  // async onClickDelete(user: any) {
+  // async onClickDelete(user: User) {
   //   const response = await this.authService.deleteUser(user);
   //   console.log(response);
   // }
