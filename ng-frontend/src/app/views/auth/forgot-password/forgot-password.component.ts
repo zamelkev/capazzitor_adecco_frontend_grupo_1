ff4c1ab4196cbe2d1ca3a4521dc8f380248722ec
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-09-05 12:11:23 +0200

    fixed feature / Forgot Password Function - Signup & 'Its' Forms Modification

--- ng-frontend/src/app/views/auth/forgot-password/forgot-password.component.ts
+++ ng-frontend/src/app/views/auth/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { AuthService } from "../../../services/auth.service";
 
 @Component({
@@ -7,9 +8,19 @@ import { AuthService } from "../../../services/auth.service";
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent {
+
+restoreForm: FormGroup
+
   constructor(
     public authService: AuthService
-  ) { }
+  ) {
+    this.restoreForm = new FormGroup({
+      email: new FormControl(),
+    });
+  }
   ngOnInit() {
+  }
+  async onRestoreSubmit() {
+    const response = await this.authService.ForgotPassword(this.restoreForm.value);
   }
 }
