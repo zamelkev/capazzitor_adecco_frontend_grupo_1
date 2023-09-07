ad9859d7f432dfe9e7d01dc2bf16a2ae46ecc208
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-09-07 18:30:19 +0200

    feature / Adding Current Candidate Data into Firestore Collections (Users & Candidates)

--- ng-frontend/src/app/main-components/cadidate/candidate-list/candidate-list.component.ts
+++ ng-frontend/src/app/main-components/cadidate/candidate-list/candidate-list.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgIterable } from '@angular/core';
+import { Candidate } from 'src/app/models/candidate.model';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-candidate-list',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./candidate-list.component.css']
 })
 export class CandidateListComponent {
+
+  user: User | any = this.authService.userData;
+
+  candidates: Candidate[] | User[] | any /*| Observable<Company[]>*/ | NgIterable<any[]> | null | undefined = [];
+
+  panelOpenState = false;
+
+  constructor(private authService: AuthService) {
+    this.candidates = [
+      {
+      uid: 1234,
+      }
+    ];
+  }
+  ngOnInit(): void {
+    this.authService?.getCandidate(this.user).subscribe( candidate => {
+      this.candidates = candidate;
+      console.log(this.candidates);
+    });
+  }
 
 }
