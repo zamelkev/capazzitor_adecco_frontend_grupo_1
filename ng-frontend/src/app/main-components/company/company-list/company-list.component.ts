b182ac373ce3c7de326141f9c3cf1d411afa4eba
Kevin Zamora <kevin.zamora.amela@gmail.com>
2023-09-07 12:41:19 +0200

    fixed feature / Displaying Logged Company Data into Company Dashboard

--- ng-frontend/src/app/main-components/company/company-list/company-list.component.ts
+++ ng-frontend/src/app/main-components/company/company-list/company-list.component.ts
@@ -19,29 +19,29 @@ export class CompanyListComponent implements OnInit {
   panelOpenState = false;
 
   constructor(private authService: AuthService) {
-    // this.companies = [
-    //   {
-    //   uid: 1234,
-    //   email: 'prueba@prueba.es',
-    //   displayName: 'Prueba',
-    //   password: '1234',
-    //   role: 'candidate',
-    //   photoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
-    //   emailVerified: true,
-    //   id: 1234,
-    //   nombreSocial: "Prueba",
-    //   nombreFiscal: "Prueba SL",
-    //   cif: "Y123456789",
-    //   telefono: 123456789,
-    //   correo: "prueba-sl@prueba-sl.es",
-    //   direcciones: "C/ Falsa, nº 10, 15L",
-    //   sector: "Automoción",
-    //   estado: "Activo",
-    //   ofertas: 15,
-    //   numeroEmpleados: 50,
-    //   proyeccion: "Nacional",
-    //   }
-    // ];
+    this.companies = [
+      {
+      uid: 1234,
+      email: 'prueba@prueba.es',
+      displayName: 'Prueba',
+      password: '1234',
+      role: 'candidate',
+      photoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
+      emailVerified: true,
+      id: 1234,
+      nombreSocial: "Prueba",
+      nombreFiscal: "Prueba SL",
+      cif: "Y123456789",
+      telefono: 123456789,
+      correo: "prueba-sl@prueba-sl.es",
+      direcciones: "C/ Falsa, nº 10, 15L",
+      sector: "Automoción",
+      estado: "Activo",
+      ofertas: 15,
+      numeroEmpleados: 50,
+      proyeccion: "Nacional",
+      }
+    ];
   }
   ngOnInit(): void {
     this.authService?.getCompany(this.user).subscribe( company => {
