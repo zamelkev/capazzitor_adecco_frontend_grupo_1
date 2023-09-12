10139d2fd8838c628b2f12f5a49e1b2c8248c771
ArielisGT <arielastumasonis@gmail.com>
2023-09-12 23:40:26 +0200

    develop: fixed offer-item component

--- ng-frontend/src/app/main-components/offer/offer-item/offer-item.component.ts
+++ ng-frontend/src/app/main-components/offer/offer-item/offer-item.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Offer } from 'src/app/models/offer.model';
+import { OfferService } from 'src/app/services/offer.service';
+import { User } from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-offer-item',
@@ -7,13 +10,15 @@ import { Offer } from 'src/app/models/offer.model';
   styleUrls: ['./offer-item.component.css'],
 })
 export class OfferItemComponent implements OnInit {
- // offerItem: any = {};
-  offers: Offer[] = [];
+  // offerItem: any = {};
+  offers?: Offer[] = [];
+  user: User | any = this.authService.user$;
   panelOpenState = true;
 
-  constructor() {
-   //this.offerItem = this.offerItem;
+  constructor(private authService: AuthService, private offerService: OfferService) {
+    //this.offerItem = this.offerItem;
     this.offers = this.offers = [
+  
       {
         title: 'Administrativo contable',
         id: 21875,
@@ -31,16 +36,23 @@ export class OfferItemComponent implements OnInit {
         salary: {
           currency: '€',
           value: 18000,
-          grossNet: 'Brutos / ',
+          grossNet: 'Brutos - ',
           period: 'año',
         },
         description:
           'Empresa internacional del sector educativo y de marketing, con sede en Elche (Alicante), busca incorporar de manera inmediata un/a ADMINISTRATIVO/A CONTABLE. Buscamos un perfil administrativo, preferiblemente con experiencia en facturación. Es necesario tener una buena disposición para trabajar en equipo de manera presencial en nuestra oficina de Elche.',
       },
-      {
-      },
+      {},
     ];
-   }
+ 
+  }
 
-  ngOnInit(): void {}
-}
+   ngOnInit(): void { }
+   
+/*   ngOnInit(): void {
+    this.offerService.GetOffersByCompany(this.user).subscribe((offers) => {
+      this.offers = offers;
+      console.log(this.offers);
+    });
+  }
+ */}
